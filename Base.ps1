e334d07665e05c051eae73117f9542458d3a4202
Nick Miller <nick.kyky@gmail.com>
2017-09-08 08:54:02 -0400

    Added Base

---
+++ Base.ps1
@@ -0,0 +1,24 @@
+### Gets & Install Chocolatey
+### Installs programs I want on ALL Windows Boxes
+## Chrome
+## VLC
+## MalwareBytes
+## atom.io
+## Virtual Box
+## Steam
+## CPU-Z
+## GoG Galaxy (choco doesn't seem to be updated but program self updates)
+## AdobeAir
+## Adobe Reader
+## Discord
+## Deluge
+## Keepass
+## Nextcloud Client
+## Office 365 Pro
+## Handbrake
+## Git / github desktop (Testing github desktop now, might use that permanently)
+### Sets execution policy
+
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install chrome vlc malwarebytes atom virtualbox steam cpu-z goggalaxy adobereader
