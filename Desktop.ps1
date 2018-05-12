3bf1c8705bfbebf043d8304ad5003635e30d402d
Nick <theflyingfool@users.noreply.github.com>
2018-05-12 17:42:23 -0400

    Fixed Desktop
    Added Air

--- Desktop.ps1
+++ Desktop.ps1
@@ -4,9 +4,10 @@
 ## MSI Afterburner
 ## Scansnap manager
 ## Intel Driver Update
+## AdobeAir
 
 choco install geforce-experience cue msiafterburner scansnapmanager hwinfo -y
 choco install intel-driver-update-utility --ignore-checksums  -y
-choco install intel-network-drivers-win10 -y
+choco install AdobeAir intel-network-drivers-win10 -y
 
 Set-ExecutionPolicy AllSigned
